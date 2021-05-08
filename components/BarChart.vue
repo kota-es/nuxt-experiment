27cc0f24d7090a4f9e1e15826244326acae61f2d
kota-es <info@k-3110.com>
2021-05-08 18:03:15 +0900

    2nd

--- components/BarChart.vue
+++ components/BarChart.vue
@@ -2,7 +2,18 @@
 import { Line } from 'vue-chartjs'
 export default {
   extends: Line,
-  props: ['chartdata', 'options'],
+  props: {
+    chartdata: {
+      type: Object
+    },
+    options: {
+      type: Object,
+      default: () => ({
+        responsive: true,
+        maintainAspectRatio: false
+      })
+    }
+  },
   mounted() {
     this.renderChart(this.chartdata, this.options)
   }
